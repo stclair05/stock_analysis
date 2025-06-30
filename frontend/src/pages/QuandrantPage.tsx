4a3ff6486cbc1a5c2231bbdc0667d7b161662260
Joshua Seet <sc05@stclair.com.sg>
2025-06-30 21:41:45 +0800

    fixed some url bugs, its working fine now

--- frontend/src/pages/QuandrantPage.tsx
+++ frontend/src/pages/QuandrantPage.tsx
@@ -1,10 +1,9 @@
 import { useEffect, useState } from "react";
-import { useNavigate, useParams } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 import Quadrant, { TableData } from "../components/Quadrant";
 import QuadrantSkeleton from "../components/QuadrantSkeleton";
 
 export default function QuadrantPage() {
-  const navigate = useNavigate();
   const { listType: listParam } = useParams<{ listType?: string }>();
 
   const [listType, setListType] = useState<"portfolio" | "watchlist">(
@@ -35,22 +34,22 @@ export default function QuadrantPage() {
     <div className="container mt-4">
       <h1 className="fw-bold mb-4">Quadrant</h1>
       <div className="mb-3">
-        <button
+        <Link
+          to="/quadrant/portfolio"
           className={`btn btn-sm me-2 ${
             listType === "portfolio" ? "btn-primary" : "btn-outline-primary"
           }`}
-          onClick={() => navigate("/quadrant/portfolio")}
         >
           Portfolio
-        </button>
-        <button
+        </Link>
+        <Link
+          to="/quadrant/watchlist"
           className={`btn btn-sm ${
             listType === "watchlist" ? "btn-primary" : "btn-outline-primary"
           }`}
-          onClick={() => navigate("/quadrant/watchlist")}
         >
           Watchlist
-        </button>
+        </Link>
       </div>
       {loading && <QuadrantSkeleton />}
       {data && !loading && <Quadrant data={data} />}
